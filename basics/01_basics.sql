d2d9df2f02fe6f301b1b3fffef81961d428f4b2c
Kunal Madoliya <madoliyakunal2005@gmail.com>
2026-08-14 12:59:16 +0530

    Add initial SQL script for database creation and table definitions

---
+++ basics/01_basics.sql
@@ -0,0 +1,84 @@
+create database amazon ;
+
+use amazon ;
+
+CREATE TABLE user (
+    name VARCHAR(100),
+    age INT,
+    dob DATE,
+    email VARCHAR(322)
+);
+
+SELECT 
+    *
+FROM
+    USER;
+
+INSERT INTO user(name , age , dob , email)
+values ("kunalmadoliya" , 21 , '2000-05-13' , "kunalmadoliya@gmail.com") ,
+("kunalmadoliya" , 21 , '2000-05-13' , "kunalmadoliya@gmail.com");
+
+
+
+create schema tinyInt_example ;
+use tinyInt_example;
+
+CREATE TABLE checktable (
+    num TINYINT
+);
+
+insert into checktable(num)
+value  (127) ;
+
+SELECT 
+    *
+FROM
+    checktable;
+
+
+create schema smallInt_example;
+use smallInt_example;
+
+CREATE TABLE check_example (
+    num SMALLINT
+);
+
+insert into check_example(num)
+values  (32768) ;
+
+SELECT 
+    *
+FROM
+    check_example;
+
+
+create schema medium_example;
+use medium_example;
+
+CREATE TABLE check_example (
+    num MEDIUMINT
+);
+
+insert into check_example(num)
+values  (-8388609) ;
+
+SELECT 
+    *
+FROM
+    check_example;
+    
+    
+create schema int_example;
+use int_example;
+
+CREATE TABLE check_example (
+    num int
+);
+
+insert into check_example(num)
+values  (-8388609) ;
+
+SELECT 
+    *
+FROM
+    check_example
